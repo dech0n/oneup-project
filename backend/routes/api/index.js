647fb416abf5e31308aa20ffa8398f5859c88907
Dechon Ryan <dechon@tuta.io>
2021-06-17 16:33:52 -0700

    Create and test login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+router.use('/session', sessionRouter);
 
-
+router.use('/users', usersRouter);
 
 module.exports = router;
