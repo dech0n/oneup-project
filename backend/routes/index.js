f745b7ec494293241807007f24e046254cc092d1
Dechon Ryan <dechon@tuta.io>
2021-06-18 09:02:41 -0700

    Create, apply and test signup validators

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,10 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.send('Hello World!');
-});
+//! testing only
+// router.get('/hello/world', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken())
+//     res.send('Hello World!');
+// });
 
 module.exports = router;
