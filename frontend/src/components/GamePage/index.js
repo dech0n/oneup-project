13b08f344e2a97ea0b6c522a682fb70a058a9e32
Dechon Ryan <dechon@tuta.io>
2021-06-22 13:46:46 -0700

    Build basic structure of game page. Needs styling and data.

--- frontend/src/components/GamePage/index.js
+++ frontend/src/components/GamePage/index.js
@@ -2,19 +2,42 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Link, NavLink } from 'react-router-dom';
 
-import * as sessionActions from '../../store/session';import './GamePage.css'
+import * as sessionActions from '../../store/session'; import './GamePage.css'
 
-function GamePage() {
+function GamePage({games}) {
 
     return (
         <>
-        <ul>
-            <li>
-                <NavLink>About</NavLink>
-                <NavLink>Events</NavLink>
-                <NavLink>Members</NavLink>
-            </li>
-        </ul>
+            <div id='group-game-header'>
+                <div id='game-image-container'>
+                    [Set game image as container bg]
+                </div>
+                <div id='basic-game-info'>
+                    <div id="game-title-container">
+                        <h2>[Game Name]</h2>
+                    </div>
+                    <div>
+                        [Platforms game can be played on]
+                    </div>
+                </div>
+            </div>
+            <ul>
+                <li>
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/events'>Events</NavLink>
+                    <NavLink to='/members'>Members</NavLink>
+                    <button>Join</button>
+                </li>
+            </ul>
+            <div id='main-content-container'>
+                <div id='game-description'>
+                    <h3>[Description Header]</h3>
+                    <p>[Description text]</p>
+                </div>
+                <div id='members-container'>
+                    [Member list as a grid. Profile pics?]
+                </div>
+            </div>
         </>
     )
 }
