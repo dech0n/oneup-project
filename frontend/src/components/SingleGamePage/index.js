160e83b3b5df8e4cde19a527f42af40d45cea027
Dechon Ryan <dechon@tuta.io>
2021-06-24 16:01:08 -0700

    Convert game header into separate GameHeader component. Apply and test component to SingleGamePage

--- frontend/src/components/SingleGamePage/index.js
+++ frontend/src/components/SingleGamePage/index.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 
 import GameEventsPage from '../GameEventsPage';
+import GameHeader from './GameHeader';
 import './SingleGamePage.css'
 
 function SingleGamePage() {
@@ -41,29 +42,7 @@ function SingleGamePage() {
     return (
         <>
             <div id='game-page-container'>
-                <div id='group-game-header'>
-                    <div id='game-image-container' >
-                        <img src={game?.image} alt='game cover art' />
-                    </div>
-                    <div id='basic-game-info'>
-                        <div id="game-title-container">
-                            <h2 id='game-title'>{game?.name}</h2>
-                        </div>
-                        <div>
-                            <h3>Play it on:</h3>
-                            <ul>
-                                {
-                                    platforms?.map(platform => (
-                                        <li key={platform.id} className='platform-bullet'><span className='platform-name'>{platform.type}</span></li>
-                                        )
-                                    )
-
-                                }
-
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                <GameHeader game={game} platforms={platforms} />
                 <div id='game-navlink-container'>
                     <ul id='game-navlinks-list'>
                         <nav id="game-navbar">
