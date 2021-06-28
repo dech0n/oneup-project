fcde87eaf56170e4efc724cdb099cce1288ef83a
Dechon Ryan <dechon@tuta.io>
2021-06-27 19:01:14 -0700

    Style event list and all event buttons

--- frontend/src/components/GameEventsPage/index.js
+++ frontend/src/components/GameEventsPage/index.js
@@ -11,9 +11,10 @@ import './Calendar.css'
 import { createRsvp } from '../../store/rsvps';
 
 function GameEventsPage({ gameId, game }) {
-     let elKey = 1000; // for element keys
-     let elKey2 = 2000; // for element keys
-     let elKey3 = 3000; // for element keys
+    let elKey = 1000; // for element keys
+    let elKey2 = 2000; // for element keys
+    let elKey3 = 3000; // for element keys
+    let elKey4 = 4000; // for element keys
     const [showNewForm, setShowNewForm] = useState(false)
     const [showEditForm, setShowEditForm] = useState(false)
     const [eventId, setEventId] = useState(null)
@@ -43,65 +44,82 @@ function GameEventsPage({ gameId, game }) {
 
     // maybe include this in the `handleSubmit` function for a new event to auto-RSVP the host
     const handleRsvp = async (eventId, platformId) => {
-        //TODO: take userId, event.platformId
-        // might have to create more gamertag seeds so every user has a gamertag on every platform
-
         const rsvpData = {
             userId,
             platformId,
             eventId,
         }
-        console.log(rsvpData) //!
         await dispatch(createRsvp(rsvpData))
     }
 
+    //TODO: change RSVP to 'leave' if already RSVP'd
+    //TODO: only show  'EDIT' / 'DELETE' buttos if user owns event
     // const eventButtons = userId === event.hostId ? (<><button>Edit</button>
     // <button>Delete</button></>) : (<button>RSVP</button>)
 
     return (
-        <div id='event-page-container'>
-            <h1>{game?.name} Events</h1>
+        <div id='event-page-container'> {/* flex */}
             <div id='calendar-container'>
                 <Calendar
                     calendarType={'US'}
                 />
+                <div id='new-event-button-container'>
+                    <button id='new-event-button'
+                            className='event-button'
+                        onClick={onlyShowNewForm}
+                    >New Event</button>
+                </div>
             </div>
-            <button
-                onClick={onlyShowNewForm}
-            >New Event</button>
-            {showEditForm ? (
-                <>
-                    <EditEventForm gameId={gameId} hostId={userId} eventId={eventId} hideForm={() => setShowEditForm(false)} />
-                </>
-            ) : null
-            }
-            {showNewForm ? (
-                <>
-                    <NewEventForm gameId={gameId} hostId={userId} hideForm={() => setShowNewForm(false)} />
-                </>
-            ) : null}
-            <ul>
-                {events?.map(event => {
-                    return showEditForm || showNewForm ? null :
-                        (
-                            <>
-                                <li key={event.name}>{event.name}</li>
-                                <li key={elKey2}>{event.date}</li>
-                                <button key={elKey3} onClick={() => onlyShowEditForm(event.id)}>Edit</button>
-
-                                <button
-                                key={event}
-                                onClick={() => handleDelete(event.id)}
-                                >Delete</button>
-
-                                <button
-                                key={++elKey}
-                                onClick={() => handleRsvp(event.id, event.platformId)}>RSVP</button>
-                            </>
-                        )
-                })
+            <div id='edit-event-form'>
+                {showEditForm ? (
+                    <>
+                        <EditEventForm gameId={gameId} hostId={userId} eventId={eventId} hideForm={() => setShowEditForm(false)} />
+                    </>
+                ) : null
                 }
-            </ul>
+            </div>
+            <div id='new-event-form'>
+                {showNewForm ? (
+                    <>
+                        <NewEventForm gameId={gameId} hostId={userId} hideForm={() => setShowNewForm(false)} />
+                    </>
+                ) : null}
+            </div>
+            <div id='event-list-container'>
+                {showEditForm || showNewForm ? null : (
+                    <h1 className='event-page' id='#event-page-header'>{game?.name} Events</h1>
+
+                )}
+                <ul id='event-list'>
+                    {events?.map(event => {
+                        return showEditForm || showNewForm ? null :
+                            (
+                                <>
+                                    <li key={event.name}><h2 className='event-header'>{event.name}<br /><span className='event-date'>{event.date}</span></h2></li>
+                                    {/* <li key={elKey2}>{event.date}</li> */}
+                                    <li key={elKey3}>{event.description}</li>
+                                    <div className='event-buttons'>
+                                        <button key={elKey4}
+                                            onClick={() => onlyShowEditForm(event.id)}
+                                            className='edit-button event-button'>Edit</button>
+
+                                        <button
+                                            key={event}
+                                            onClick={() => handleDelete(event.id)}
+                                            className='delete-button event-button'
+                                        >Delete</button>
+
+                                        <button
+                                            key={++elKey}
+                                            onClick={() => handleRsvp(event.id, event.platformId)}
+                                            className='rsvp-button event-button'>RSVP</button>
+                                    </div>
+                                </>
+                            )
+                    })
+                    }
+                </ul>
+            </div>
         </div >
     )
 }
