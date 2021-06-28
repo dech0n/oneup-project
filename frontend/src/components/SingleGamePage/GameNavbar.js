72aba19f8c47ab077bb8f8e3cb83fa41f1610a76
Dechon Ryan <dechon@tuta.io>
2021-06-27 22:48:36 -0700

    Add more styling

--- frontend/src/components/SingleGamePage/GameNavbar.js
+++ frontend/src/components/SingleGamePage/GameNavbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import {createUserGame} from '../../store/userGames'
+import { NavLink, Redirect, useHistory } from 'react-router-dom';
+import { createUserGame } from '../../store/userGames'
 
-function GameNavbar({gameId}) {
+function GameNavbar({ gameId }) {
+    // const history = useHistory()
+    const user = useSelector(state => state.session.user)
+    // if (!user) {
+    //     history.push('/login')
+    // }
+
     const dispatch = useDispatch();
-    const userId = useSelector(state => state.session.user.id)
+    const userId = useSelector(state => state.session.user?.id)
 
     const handleJoin = async () => {
         const userGameData = {
@@ -16,17 +22,29 @@ function GameNavbar({gameId}) {
         await dispatch(createUserGame(userGameData))
         // something here to change the appearance of the join button (i.e. "leave" button ?)
     }
+
     return (
         <div id='game-navlink-container'>
             <ul id='game-navlinks-list'>
                 <nav id="game-navbar">
                     <li className='game-navlinks-items'>
-                        <NavLink className='game-navlink' to={`/games/${gameId}`}>About</NavLink>
-                        <NavLink className='game-navlink' to={`/games/${gameId}/events`}>Events</NavLink>
-                        <NavLink className='game-navlink' to='/members'>Members</NavLink>
-                        <button
-                        id='join-button'
-                        onClick={handleJoin}>Join</button>
+                        <NavLink
+                            activeClassName='about-active'
+                            exact to={`/games/${gameId}`}
+                            className='game-navlink'>About</NavLink>
+                        <NavLink
+                            activeClassName='events-active'
+                            to={`/games/${gameId}/events`}
+                            className='game-navlink'>Events</NavLink>
+                        <NavLink
+                            to='/members'
+                            className='game-navlink'>Members</NavLink>
+                        {user ? (
+                            <button
+                                id='join-button'
+                                onClick={handleJoin}>Join</button>
+                        ) : null
+                        }
                     </li>
                 </nav>
             </ul>
