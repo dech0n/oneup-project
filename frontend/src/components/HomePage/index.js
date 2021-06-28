38b6415eba317a704d3cc8e7e2f6435c935510ae
Dechon Ryan <dechon@tuta.io>
2021-06-27 17:14:39 -0700

    Style homepage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getGames } from '../../store/games';
 
 import './HomePage.css'
@@ -17,21 +18,27 @@ function HomePage() {
         <div id='homepage-container'>
             <div id='intro-block'>
                 <div id='intro-to-site'>
-                    <h1>So many games, so little time!</h1>
-                    <p>Join a game to find other players and schedule online gaming events.</p>
+                    <h1 className='header'><span>Dive in!</span> There are so many players to meet on One<span>Up</span></h1>
+                    <p>Join a game to find other players. Help your fellow gamers fill their lobbies or schedule a session and invite them to yours.</p>
                 </div>
                 <div id='intro-image'>
-                    <p>[Place Intro Image Here]</p>
+                    <img id='splash-image' src='./images/grey-gamepad.png' alt='gamer-zone' />
                 </div>
             </div>
             <div id='list-div'>
                 <ul>
                     <div className='game-div'>
                         {games?.map(game => (
+                            <Link className='link' to={`/games/${game?.id}`}>
                             <div className='single-game-div'>
-                                <li key={game.image}><img src={game.image} alt='game cover art' /></li>
-                                <li key={game.id}>{game.name}</li>
+                                <li className='game-image' key={game.image}>
+                                    <img src={game.image} alt='game cover art' />
+                                </li>
+                                <li className='game-title' key={game.id}>
+                                    {game.name}
+                                </li>
                             </div>
+                            </ Link>
                         ))}
                     </div>
                 </ul>
